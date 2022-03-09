0b527ea4c1d1d72ddfad2cd9e40a54eac277e913
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-09 12:41:48 -0800

    Added count++s and some style code

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 // set state for all of the character's catchphrases
+const catchPhrases = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
-    // increment the head change count state
     
+    // increment the head change count state
+    headCount++;
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `/assets/${headDropdown.value}-head.png`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
@@ -28,9 +32,9 @@ middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
 
     // increment the middle change count state
-    
+    middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `/assets/${middleDropdown.value}-head.png`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
@@ -39,15 +43,15 @@ bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
 
     // increment the bottom change count state
-    
+    bottomCount++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomEl.style.backgroundImage = `/assets/${bottomDropdown.value}-head.png`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    
+    const newCatchPhrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
 
     // clear out the form input's value so it's empty to the user
